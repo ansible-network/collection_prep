2ffdf05c2e384031ccb69211b0d99c2ab0ab7b93
Bradley A. Thornton <bthornto@thethorntons.net>
2020-05-07 09:17:51 -0700

    remove LCI

--- update_rb.py
+++ update_rb.py
@@ -18,28 +18,6 @@ logging.basicConfig(format="%(levelname)-10s%(message)s", level=logging.INFO)
 SUBDIRS = ("modules", "action")
 SPECIALS = {"ospfv2": "OSPFv2", "interfaces": "Interfaces", "static": "Static"}
 
-
-LICENSE = """
-#!/usr/bin/python
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
-"""
-
-
 def load_py_as_ast(path):
     """
     Load a file as an ast object
