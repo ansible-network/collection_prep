ansible-network/collection_prep

commit 28ec65e5e4f56291f682436eed3cf62e3a51f6c1

Ensure there is a single newline after the plugin description, if present (#52)
* Ensure there is a single newline after the plugin description, if present
* Also only set description if docstring is not empty

collection_prep/cmd/add_docs.py
@@ -143,6 +143,7 @@ def update_readme(content, path, gh_url, branch_name):
             )
             if "_description" in plugins:
                 data.append(plugins.pop("_description"))
+                data.append("")
         data.append("Name | Description")
         data.append("--- | ---")
         for plugin, info in sorted(plugins.items()):
@@ -230,7 +231,9 @@ def handle_simple(collection, fullpath, kind):
     if not classdef:
         return plugins
     else:
-        plugins["_description"] = ast.get_docstring(classdef[0], clean=True)
+        docstring = ast.get_docstring(classdef[0], clean=True).strip()
+        if docstring:
+            plugins["_description"] = docstring
 
     simple_map = next(
         (

commit b2d061c729571b80cae65c86c9c617a0a6c9daad

prevent strip() on empty docstring
fix bug introduced in 28ec65e

collection_prep/cmd/add_docs.py
@@ -231,9 +231,9 @@ def handle_simple(collection, fullpath, kind):
     if not classdef:
         return plugins
     else:
-        docstring = ast.get_docstring(classdef[0], clean=True).strip()
+        docstring = ast.get_docstring(classdef[0], clean=True)
         if docstring:
-            plugins["_description"] = docstring
+            plugins["_description"] = docstring.strip()
 
     simple_map = next(
         (