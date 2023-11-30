95feac12157acc5e2e11be5c7d1404279cf5e502
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2023-11-30 17:28:17 +0530

    Update collection_prep/cmd/add_docs.py
    Co-authored-by: Sagar Paul <sagpaul@redhat.com>

--- collection_prep/cmd/add_docs.py
+++ collection_prep/cmd/add_docs.py
@@ -59,7 +59,7 @@ SUBDIRS = (
 TEMPLATE_DIR = os.path.dirname(__file__)
 ANSIBLE_COMPAT = """## Ansible version compatibility
 
-This collection has been tested against following Ansible versions: **{requires_ansible}**.
+This collection has been tested against the following Ansible versions: **{requires_ansible}**.
 
 Plugins and modules within a collection may be tested with only specific Ansible versions.
 A collection may contain metadata that identifies these versions.
